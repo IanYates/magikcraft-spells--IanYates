478d4b114b54e79b72d40fd57d8df6602a194baf
IanYates <ian@medicalit.com.au>
2017-12-01 10:29:11 +0000

    Update r.ts

--- r.ts
+++ r.ts
@@ -4,9 +4,6 @@ function r(t = 5) {
 	const times = t * 1000 / 300;
     let n = times;
 
-    magik.setTimeout(() => shield(), 300);
-    magik.dixit('magical shield');
-
     function shield(){
         n--;
         const location = magik.hic();
@@ -19,6 +16,10 @@ function r(t = 5) {
         }
 
     }
+    
+    magik.setTimeout(() => shield(), 300);
+    magik.dixit('magical shield');
+
 }
 
 function toss(entity){
