00e6e9f9ed45b7636b6bf889852bae5b6a4793ae
IanYates <ian@medicalit.com.au>
2017-12-01 10:27:06 +0000

    Update r.ts

--- r.ts
+++ r.ts
@@ -4,12 +4,28 @@ function r(t = 5) {
 	const times = t * 1000 / 300;
     let n = times;
 
-    magik.setTimeout(shield, 300);
+    magik.setTimeout(() => shield(), 300);
     magik.dixit('magical shield');
 
     function shield(){
         n--;
         const location = magik.hic();
-        const nearbyEntities = location.getWorld().strikeLightning(location);
+        const nearbyEntities = location.getWorld().getNearbyEntities(location, 8,8,8);
+        nearbyEntities.forEach(entity=>toss(entity));
+        if (n > 0) {
+            magik.setTimeout(shield, 300);
+        } else {
+            magik.dixit('Sheild out!');
+        }
+
     }
+}
+
+function toss(entity){
+    if (entity.getName() == magik.getSender().getName()){
+        return;
+    }
+
+    var Vector = Java.type('org.bukkit.util.Vector');
+    entity.setVelocity(new Vector(2,2,2));
 }
